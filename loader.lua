d24b44447f205f7b43c12de04c75b7749618f536
amabar49-boop <amabar49@gmail.com>
2026-01-28 19:28:51 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,33 +1,9 @@
--- Fish It Hub | Loader
--- Obsidian UI
--- github.com/USERNAME/REPO
+local url = "https://raw.githubusercontent.com/amabar49-boop/silverhub/main/main.lua"
 
-local BASE_URL = "https://raw.githubusercontent.com/amabar49-boop/silverhub/main/"
+local ok, err = pcall(function()
+    loadstring(game:HttpGet(url))()
+end)
 
--- anti double execute
-if getgenv().FISHIT_LOADER then
-    return
+if not ok then
+    warn("Loader error:", err)
 end
-getgenv().FISHIT_LOADER = true
-
-local function safeLoad(file)
-    local ok, data = pcall(function()
-        return game:HttpGet(BASE_URL .. file)
-    end)
-
-    if not ok or not data then
-        warn("[Fish It] Gagal HttpGet:", file)
-        return
-    end
-
-    local func, err = loadstring(data)
-    if not func then
-        warn("[Fish It] Loadstring error:", err)
-        return
-    end
-
-    func()
-end
-
--- load main UI
-safeLoad("main.lua")
