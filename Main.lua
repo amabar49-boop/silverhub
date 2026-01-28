e7e07949ac16666fd429d10e651d6671e4c4b69e
amabar49-boop <amabar49@gmail.com>
2026-01-28 19:08:40 +0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,30 @@
+-- Fish It Hub | Main UI (Obsidian)
+
+local Obsidian = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Obsidian.lua"
+))()
+
+local Window = Obsidian:CreateWindow({
+    Title = "Fish It Hub",
+    Footer = "Silver Hub",
+    ToggleKey = Enum.KeyCode.RightShift
+})
+
+local LoaderTab = Window:CreateTab("Loader", "home")
+
+LoaderTab:AddButton({
+    Title = "Load Fish It Script",
+    Description = "Auto Fish • Auto Reel • AFK",
+    Callback = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/silverhub/amabar49-boop/main/fishit.lua"
+        ))()
+    end
+})
+
+LoaderTab:AddButton({
+    Title = "Unload UI",
+    Callback = function()
+        game:GetService("CoreGui").Obsidian:Destroy()
+    end
+})
